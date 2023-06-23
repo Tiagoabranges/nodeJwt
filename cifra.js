dee34d0c0b0812cde5a3fcd318150ab2d79d29dd
tiagoabranges <tiagoabranges@gmail.com>
2023-06-23 13:11:33 -0300

    first commit

---
+++ cifra.js
@@ -0,0 +1,14 @@
+const mensagemSecreta = "minhamensagemsecreta";
+
+//código omitido
+
+function cifraMensagem(mensagem, movimentos){
+    const mensagemCifrada = mensagem.split('').map( caractere => {
+        const codigoCaractere = caractere.charCodeAt(0);
+        return String.fromCharCode( codigoCaractere + movimentos)
+    })
+
+    return mensagemCifrada.join('')
+}
+
+console.log(cifraMensagem(mensagemSecreta, 3))
